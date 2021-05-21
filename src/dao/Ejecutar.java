ebdecba40c667ce163667f644bc73303680c6847
Gabriel Velez <gabriel_velez-30@outlook.com>
2021-05-20 22:01:11 -0300

    primer commit netbeans

---
+++ src/dao/Ejecutar.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Leirbag
+ */
+public class Ejecutar {
+    private ConexionBD conectar;
+    private Connection conexion;
+    private Statement stmt = null;
+    
+    public  ResultSet consulta(String query) throws SQLException{
+        conectar = new ConexionBD();
+        conexion = conectar.conexion();
+        ResultSet res = null;
+        if(conexion != null){
+            try{
+                stmt = conexion.createStatement();
+                res = stmt.executeQuery(query);
+                return res;
+            }
+            catch(SQLException ex){
+                throw ex;
+            }
+        }
+        return res;
+    }
+    
+    public String peticion(String query){
+        conectar = new ConexionBD();
+        conexion = conectar.conexion();
+        if(conexion != null){
+            try{
+                stmt = conexion.createStatement();
+                stmt.executeUpdate(query);
+                
+                
+            }
+            catch(SQLException ex){
+                try{
+                    if(stmt == null)
+                        conexion.rollback();
+                    return "Falló en la actualización";
+                }
+                catch(SQLException se){
+                    return "Rollback realizado";
+                }
+            }
+        }
+        return "";
+    }
+}
