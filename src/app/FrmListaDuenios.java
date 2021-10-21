a01618ea38d53c8f8aa51d751a58772a9ae71349
Gabriel Velez <gabriel_velez-30@outlook.com>
2021-10-21 19:33:12 -0300

    downgrade jdk 1.8

--- src/app/FrmListaDuenios.java
+++ src/app/FrmListaDuenios.java
@@ -25,16 +25,7 @@ public class FrmListaDuenios extends javax.swing.JInternalFrame {
 
     FrmMenuPrincipal mPrincipal;
     
-    private void load() throws PropertyVetoException{
-        try{
-            this.setMaximum(true);
-        }
-        catch(PropertyVetoException e)
-        {
-            JOptionPane.showMessageDialog(null, e.toString());
-        }
-        
-        
+    private void load(){
         loadTable();
     }
     
